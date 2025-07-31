e33910ec376bc15f5dc831d366fd0437181c40c3
Jacques Carlos <jacquescarlos05@gmail.com>
2025-07-31 20:08:13 -0300

    Exercício 57 e 58

--- Python Exercícios/Exercício.057.py
+++ Python Exercícios/Exercício.057.py
@@ -3,3 +3,13 @@
     Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" ou "F". Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 """
+
+sexo = 0
+
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('Digite seu sexo [M/F]: ')).upper()
+
+if sexo == 'M':
+    print('Seu sexo é masculino')
+elif sexo == 'F':
+    print('Seu sexo é feminino')
---
+++ Python Exercícios/Exercício.058.py
@@ -0,0 +1,25 @@
+"""
+[Exercício 58]
+    Melhore o jogo do DESAFIO 28 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
+    [Exercício 28]
+        Escreva um programa que faça o computador "pensar" em número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.   
+        O programa deverá escrever na tela se o usuário venceu ou perdeu.    
+
+"""
+from random import randint
+
+computador = randint(0,10)
+usuario = 11
+tentativas = 0
+
+while computador != usuario:
+    print('Estou pensando em um número entre 0 e 10... Tente adivinhar!')
+    usuario = int(input('Digite seu palpite: '))
+    if usuario > 10:
+        print('Por favor, informe um número válido.')
+    else:
+        tentativas += 1
+        if computador != usuario:
+            print('Que pena, você errou!')
+print(f'Parabéns, você acertou! Você precisou de {tentativas} tentativa(s).')
