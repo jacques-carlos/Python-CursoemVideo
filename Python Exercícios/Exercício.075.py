76a51857fd2b75e9ed3df0eaa648eb08cedb6463
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-20 09:28:20 -0300

    ex 75 e ex 76 (incomp)

--- Python Exercícios/Exercício.075.py
+++ Python Exercícios/Exercício.075.py
@@ -8,3 +8,36 @@
     
 """
 
+while True:
+    n1 = int(input('Digite um número entre 0 e 10: '))
+    if 0 >= n1 < 11:
+        if n1 % 2 == 0:
+            p1 = n1
+        break
+
+while True:
+    n2 = int(input('Digite outro número entre 0 e 10: '))
+    if 0 >= n2 < 11:
+        if n2 % 2 == 0:
+            p2 = n2
+        break
+
+while True:
+    n3 = int(input('Digite mais outro número entre 0 e 10: '))
+    if 0 >= n3 < 11:
+        if n3 % 2 == 0:
+            p3 = n3
+        break
+
+while True:
+    n4 = int(input('Digite um último número entre 0 e 10: '))
+    if 0 >= n4 < 11:
+        if n4 % 2 == 0:
+            p4 = n4
+        break
+
+tupla = (n1, n2, n3, n4)
+pares = ()
+
+print(f'O número 9 apareceu {tupla.count('9')} vezes.')
+print(f'O primeiro número 3 apareceu na {tupla.index('3')}ª posição.')
---
+++ Python Exercícios/Exercício.076.py
@@ -0,0 +1,44 @@
+"""
+[Exercício 76]
+    Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+
+    No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+
+"""
+
+x=0
+tupla = ('Caderno', 24.90, 'Lápis', 0.99, 'Caneta', 1.99, 'Borracha', 2.49, 'Pasta', 4.99, 'Mochila', 199.90, 'Estojo', 19.90)
+
+print('-'*70)
+print(f'{'LISTAGEM DE PREÇOS':^70}')
+print('-'*70)
+
+print (tupla[0], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[1]:0.2f}')
+
+print (tupla[2], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[3]:0.2f}')
+
+print (tupla[4], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[5]:0.2f}')
+
+print (tupla[6], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[7]:0.2f}')
+
+print (tupla[8], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[9]:0.2f}')
+
+print (tupla[10], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[11]:0.2f}')
+
+print (tupla[12], end='')
+print(f'{'R$':.>60}', end='')
+print(f'{tupla[13]:0.2f}')
+
+print('-'*70)
