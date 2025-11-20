f3aea92f7f3a9b8b050627624a5531c66ad8f31c
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-20 15:03:29 -0300

    ex76 refeito

--- Python Exercícios/Exercício.076.py
+++ Python Exercícios/Exercício.076.py
@@ -6,39 +6,14 @@
 
 """
 
-x=0
 tupla = ('Caderno', 24.90, 'Lápis', 0.99, 'Caneta', 1.99, 'Borracha', 2.49, 'Pasta', 4.99, 'Mochila', 199.90, 'Estojo', 19.90)
 
-print('-'*70)
-print(f'{'LISTAGEM DE PREÇOS':^70}')
-print('-'*70)
+print('-'*50)
+print(f'{'LISTAGEM DE PREÇOS':^50}')
+print('-'*50)
 
-print (tupla[0], end='')
-print(f'{'R$':.>60}', end='')
-print(f'{tupla[1]:0.2f}')
-
-print (tupla[2], end='')
-print(f'{'R$':.>60}', end='')
-print(f'{tupla[3]:0.2f}')
-
-print (tupla[4], end='')
-print(f'{'R$':.>60}', end='')
-print(f'{tupla[5]:0.2f}')
-
-print (tupla[6], end='')
-print(f'{'R$':.>60}', end='')
-print(f'{tupla[7]:0.2f}')
-
-print (tupla[8], end='')
-print(f'{'R$':.>60}', end='')
-print(f'{tupla[9]:0.2f}')
-
-print (tupla[10], end='')
-print(f'{'R$':.>60}', end='')
-print(f'{tupla[11]:0.2f}')
-
-print (tupla[12], end='')
-print(f'{'R$':.>60)}', end='')
-print(f'{tupla[13]:0.2f}')
-
-print('-'*70)
+for p in range (0, len(tupla)):
+    if p % 2 == 0:
+        print(f'{tupla[p]:.<40}', end='')
+    if p % 2 == 1:
+        print(f'R${tupla[p]:7.2f}')
