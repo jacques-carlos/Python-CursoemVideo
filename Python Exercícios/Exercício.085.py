2b78a9e6a6c25d0413d0f2b7600d73a6cbc6ef03
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-22 19:49:00 -0300

    ex85

--- Python Exercícios/Exercício.085.py
+++ Python Exercícios/Exercício.085.py
@@ -4,3 +4,16 @@
     No final, mostre os valores pares e ímpares em ordem crescente.
 
 """
+
+lista = [[], []]
+
+for c in range (1, 8): 
+    valor = int(input(f'[{c}/7] Digite um valor: '))
+    if valor % 2 == 0:
+        lista[0].append(valor)
+    else:
+        lista[1].append(valor)
+lista[0].sort()
+lista[1].sort()
+print(f'Os valores PARES são: {lista[0]}')
+print(f'Os valores ÍMPARES são: {lista[1]}')
