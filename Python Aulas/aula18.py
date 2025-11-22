d9439ca66ed8f10f51abf0c8fb5276b54d525d74
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-22 18:57:07 -0300

    ex84

---
+++ Python Aulas/aula18.py
@@ -0,0 +1,56 @@
+"""
+**************************************************
+AULA 18
+**************************************************
+
+        LISTAS
+        Parte 2
+
+"""
+
+dadosP = list()
+dadosP.append('Pedro')
+dadosP.append(25)
+
+dadosM = list()
+dadosM.append('Maria')
+dadosM.append(19)
+
+dadosJ = list()
+dadosJ.append('João')
+dadosJ.append(32)
+
+pessoas = list()
+pessoas.append(dadosP[:])
+pessoas.append(dadosM[:])
+pessoas.append(dadosJ[:])
+
+#ou
+
+galera = [['Pedro', 25], ['Maria', 19], ['João', 32], ['Ana', 45]]
+
+print(galera[0][0])
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade')
+
+dados = list()
+pessoal = list()
+
+# Input
+
+maiores = menores = 0
+
+for c in range(0,3):
+    pessoal.append(str(input('Digite o nome: ')))
+    pessoal.append(int(input('Digite a idade: ')))
+    dados.append(pessoal[:])
+    pessoal.clear()
+print(dados)
+for x in dados:
+    if x[1] >= 21:
+        print(f'{x[0]} é maior de idade com {x[1]} anos')
+        maiores += 1
+    else:
+        print(f'{x[0]} é menor de idade com {x[1]} anos')
+        menores += 1
+print(f'Há {maiores} maiores de idade e {menores} menores de idade')
--- Python Exercícios/Exercício.083.py
+++ Python Exercícios/Exercício.083.py
@@ -2,7 +2,7 @@
 [Exercício 83]
     Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
     Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
-15 + 7 * 3
+    
 """
 
 expre = 'correta'
