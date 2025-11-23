fe621ff0e0ff19b22c31df935ad4d30b5bd6eb98
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-23 15:12:41 -0300

    ex89 update

--- Python Exercícios/Exercício.089.py
+++ Python Exercícios/Exercício.089.py
@@ -5,43 +5,30 @@
 """
 cont=0
 boletim = []
-
 while True:
-    cont+=1
-    aluno = []
-    notas = []
     nome = str(input('Nome: '))
     nota1 = float(input('Nota 1: '))
     nota2 = float(input('Nota 2: '))
-    aluno.append(nome)
-    notas.append(nota1)
-    notas.append(nota2)
-    aluno.append(notas[:])
-    boletim.append(aluno[:])
-    notas.clear()
-    aluno.clear()
+    boletim.append([nome, [nota1, nota2]])
     continuar = str(input('Deseja continuar? [S/N]: ')).strip()[0]
     while continuar not in 'SsNn':
         continuar = str(input('Resposta inválida. Digite [S] para SIM e [N] para NÃO: ')).strip()[0]
     if continuar in 'Nn':
         break
-
+print('')
+print('='*60)
+print(f'{'Num.':<5}{'NOME':<15}{'MÉDIA':>5}')
+for num, aluno in enumerate(boletim):
+    print('-'*25)
+    print(f'{num:<5}{aluno[0]:<15}{(aluno[1][0]+aluno[1][1])/2:>5.1f}')
+print('='*60)
 while True:
-    print('')
-    print('='*60)
-    print(f'{'Num.':<5}{'NOME':<15}{'MÉDIA':>5}')
-    for num in range (0, cont):
-        print('-'*25)
-        print(f'{num:<5}{boletim[num][0]:<15}{(boletim[num][1][0]+boletim[num][1][1])/2:>5.1f}')
-    print('='*60)
-    while True:
-        detalhe = int(input('\nMostrar notas de qual aluno? (999 interrompe): '))
-        if detalhe == 999:
-            break
-        if detalhe in range (0, cont):
-            print(f'As notas de {boletim[detalhe][0]} são {boletim[detalhe][1]}')
-        else:
-            print('Número inválido. Por favor, tente novamente')
-    print('='*60)
-    print('Finalizando...'.upper())
-    break
+    detalhe = int(input('\nMostrar notas de qual aluno? (999 interrompe): '))
+    if detalhe == 999:
+        print('Finalizando...'.upper())
+        break
+    if detalhe in range (0, len(boletim)):
+        print(f'As notas de {boletim[detalhe][0]} são {boletim[detalhe][1]}')
+    else:
+        print('Número inválido. Por favor, tente novamente')
+print('='*60)
