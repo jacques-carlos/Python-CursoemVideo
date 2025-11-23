dc06a85ecdde526ad6d216f07a50e99010df0b52
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-22 22:41:45 -0300

    ex89 ufa

---
+++ Python Exercícios/Exercício.088.py
@@ -0,0 +1,29 @@
+"""
+[Exercício 88]
+    Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+    O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+
+"""
+
+from random import randint
+from time import sleep
+jogo = []
+jogos = []
+quantidade = int(input('Quantos jogos você deseja que eu faça? '))
+for etapa in range (0, quantidade):
+    cont = 0 #existem 0 números nesse jogo
+    while True:
+        num = randint(1,60)
+        if num not in jogo: #elimina repetições
+            jogo.append(num)
+            cont += 1
+        if cont == 6:
+            break
+    jogo.sort()
+    jogos.append(jogo[:])
+    jogo.clear()
+
+for x in range(0, quantidade):
+    print(f'Jogo {x+1}: {jogos[x]}')
+    sleep(1)
+print('Boa sorte e jogue com responsabilidade!'.upper())
---
+++ Python Exercícios/Exercício.089.py
@@ -0,0 +1,50 @@
+"""
+[Exercício 89]
+    Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
+    
+    No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar notas de cada alunos individualmente.
+
+"""
+cont=0
+boletim = []
+
+while True:
+    cont+=1
+    aluno = []
+    notas = []
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    aluno.append(nome)
+    notas.append(nota1)
+    notas.append(nota2)
+    aluno.append(notas[:])
+    boletim.append(aluno[:])
+    notas.clear()
+    aluno.clear()
+    continuar = str(input('Deseja continuar? [S/N]: ')).strip()[0]
+    while continuar not in 'SsNn':
+        continuar = str(input('Resposta inválida. Digite [S] para SIM e [N] para NÃO: ')).strip()[0]
+    if continuar in 'Nn':
+        break
+
+while True:
+    print('')
+    print('='*60)
+    print(f'{'Num.':<5}{'NOME':<15}{'MÉDIA':>5}')
+    for num in range (0, cont):
+        print('-'*25)
+        print(f'{num:<5}{boletim[num][0]:<15}{(boletim[num][1][0]+boletim[num][1][1])/2:>5.1f}')
+    print('='*60)
+    while True:
+        detalhe = int(input('\nMostrar notas de qual aluno? (999 interrompe): '))
+        if detalhe == 999:
+            break
+        if detalhe in range (0, cont):
+            print(f'As notas de {boletim[detalhe][0]} são {boletim[detalhe][1]}')
+        else:
+            print('Número inválido. Por favor, tente novamente')
+    print('='*60)
+    print('Finalizando...'.upper())
+    break
+    
