051836813bac45552937ee58ad70e4777c297ff3
Jacques Carlos <jacquescarlos05@gmail.com>
2025-11-26 21:28:19 -0300

    ex95 ex93upd

--- Python Exercícios/Exercício.093.py
+++ Python Exercícios/Exercício.093.py
@@ -7,23 +7,20 @@
 """
 jogador = dict()
 partidas = list()
-total = 0
 jogador['nome'] = str(input('Nome: '))
 quantidade = int(input('Quantas partidas jogou? '))
 for partida in range (0, quantidade):
     partidas.append(int(input(f'Quantos gols na partida {partida}? ')))
 jogador['gols'] = partidas[:]
-for gols in partidas:
-    total += gols
-jogador['total'] = total
-print('='*50)
+jogador['total'] = sum(partidas)
+print('='*60)
 print(jogador)
-print('='*50)
+print('='*60)
 for k,v in jogador.items():    
     print(f'O campo {k} tem o valor {v}.')
-print('='*50)
+print('='*60)
 print(f'O jogador {jogador['nome']} jogou {quantidade} partidas.')
 for c, p in enumerate(partidas):
     print(f' => Na partida {c}, fez {p} gols.')
-print(f'Foi um total de {total} gols.')
-print('='*50)
+print(f'Foi um total de {jogador['total']} gols.')
+print('='*60)
--- Python Exercícios/Exercício.095.py
+++ Python Exercícios/Exercício.095.py
@@ -1,7 +1,47 @@
 """
 [Exercício 95]
-    Crie um programa que gerencie o aproveitamento de um jogador de futebol.
-    O programa vai ler o nome do jogador e quantas partidas ele jogou.
-    Depois vai ler a quantidade de gols feitos em cada partida.
-    No final isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+    Aprimore o DESAFIO 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
 """
+lista = list()
+jogador = dict()
+partidas = list()
+while True:
+    total = 0
+    jogador['nome'] = str(input('Nome: '))
+    quantidade = int(input('Quantas partidas jogou? '))
+    for p in range (0, quantidade):
+        partidas.append(int(input(f'Quantos gols na partida {p}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    lista.append(jogador.copy())
+    jogador.clear()
+    partidas.clear()
+    resposta = str(input('Deseja continuar? [S/N]: ')).strip()[0]
+    while resposta not in 'SsNn':
+        resposta = str(input('Tente novamente. Deseja continuar? [S/N]: ')).strip()[0]
+    if resposta in 'Nn':
+        break
+
+print('='*60)
+print(f'{'cod.':<5}{'nome':<10}{'gols':<15}{'total':>5}')
+for c, j in enumerate(lista):
+    print(f'{c:<5}{j['nome']:<10}{j['gols']}{j['total']:>5}')
+print('='*60)
+
+while True:
+    x = int(input('Mostrar dados de qual jogador? '))
+    if x == 999:
+        print('Encerrando o programa...')
+        break
+    else:
+        while x not in range (0, len(lista)):
+            print('Erro! Tente novamente.')
+            x = int(input('Mostrar dados de qual jogador? '))
+        
+        print(f'O jogador {lista[x]['nome']} jogou {len(lista[x]['gols'])} partidas.')
+        for c, p in enumerate(lista[x]['gols']):
+            print(f' => Na partida {c}, fez {p} gols.')
+        print(f'Foi um total de {lista[x]['total']} gols.')
+        print('='*60)
+print('='*60)
+    
