c68eab0cf4aab6fca4e1f65e759d877f430e39e4
Jacques Carlos <jacquescarlos05@gmail.com>
2025-12-01 18:10:32 -0300

    ex97

--- Python Exercícios/Exercício.097.py
+++ Python Exercícios/Exercício.097.py
@@ -1,4 +1,20 @@
 """
 [Exercício 96]
-    Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terrreno retangular (largura e comprimento) e mostre a área do terreno.
+    Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+
+    Ex:
+            escreva('Olá, Mundo!')
+    Saída:
+            ===========
+            Olá, Mundo!
+            ===========
 """
+def escreva(texto):
+    tamanho = len(texto)
+    print('=' * tamanho)
+    print(texto)
+    print('=' * tamanho)
+
+escreva('Testando... 1... 2... 3...')
+escreva('Olá, Mundo!')
+escreva(str(input('Digite seu texto: ')))
