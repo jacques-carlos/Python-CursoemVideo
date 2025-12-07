b516ab941037ad51b88ec16b56613df0f284c8da
Jacques Carlos <jacquescarlos05@gmail.com>
2025-12-06 21:15:44 -0300

    aula21

---
+++ Python Aulas/aula21b.py
@@ -0,0 +1,23 @@
+"""
+**************************************************
+AULA 21 parte b
+**************************************************
+        FATORIAL DE UM NÚMERO
+"""
+
+def fatorial(num=1):
+        total = 1
+        while True:        
+                total *= num
+                num -= 1
+                if num <= 1:
+                        break
+        return(total)
+
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é igual a {fatorial(n)}')
+
+f1 = fatorial(5) 
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}')
---
+++ Python Aulas/aula21c.py
@@ -0,0 +1,17 @@
+"""
+**************************************************
+AULA 21 parte c
+**************************************************
+        PAR OU ÍMPAR
+"""
+def par(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+    
+num = int(input('Digite um número: '))
+if par(num):
+    print('Esse número é par!')
+else:
+    print('Esse número é ímpar!')
