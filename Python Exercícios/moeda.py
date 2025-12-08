6598e80d0634432c6952c0431ab10d586fd312cc
Jacques Carlos <jacquescarlos05@gmail.com>
2025-12-08 16:17:50 -0300

    ex109

--- Python Exercícios/moeda.py
+++ Python Exercícios/moeda.py
@@ -1,17 +1,31 @@
-def aumentar(valor, porcentagem):
-    return valor + (valor*porcentagem)/100
+def aumentar(valor, porcentagem, format=False):
+    resultado = valor + (valor*porcentagem)/100
+    if format:
+        return f'R${resultado:0.2f}'
+    else:
+        return resultado
+
+def diminuir(valor, porcentagem, format=False):
+    resultado = valor - (valor*porcentagem)/100
+    if format:
+        return f'R${resultado:0.2f}'
+    else:
+        return resultado
 
 
-def diminuir(valor, porcentagem):
-    return valor - (valor*porcentagem)/100 
+def dobro(valor, format=False):
+    if format:
+        return f'R${valor * 2:0.2f}'
+    else:    
+        return valor * 2
 
 
-def dobro(valor):
-    return valor * 2
+def metade(valor, format=False):
+    if format:
+        return f'R${valor / 2:0.2f}'
+    else:    
+        return valor / 2
 
-
-def metade(valor):
-    return valor / 2
 
 def moeda(valor):
     return f'R${valor:0.2f}'
