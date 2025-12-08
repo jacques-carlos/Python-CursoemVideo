16c4f7c01794dc5f604f2e2ad9bcd57a9e7c13dc
Jacques Carlos <jacquescarlos05@gmail.com>
2025-12-08 15:25:29 -0300

    aula22

---
+++ Python Módulos/cálculos.py
@@ -0,0 +1,6 @@
+from úteis import números
+
+num = int(input('Digite um valor: '))
+print(f'O fatorial de {num} é {números.fatorial(num)}')
+print(f'O dobro de {num} é {números.dobro(num)}')
+print(f'O triplo de {num} é {números.triplo(num)}')
